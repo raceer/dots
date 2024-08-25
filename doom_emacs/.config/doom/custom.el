f6a03ef0fc7aa0e4109330bb8c9f1691a7c3451b
raceer <web.raceer@pm.me>
2024-08-25 13:50:17 +0300

    Doom Added

---
+++ doom_emacs/.config/doom/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-custom-commands
+   '(("n" "Agenda and all TODOs"
+      ((agenda "" nil)
+       (alltodo "" nil))
+      nil)))
+ '(org-agenda-files
+   '("~/510_University/18.01_SingleVariableCalculus/Notes.org" "/home/raceer/110_Syncthing/210_Notes/050_orgTasks/personal.org"))
+ '(package-selected-packages '(org-fragtog)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
