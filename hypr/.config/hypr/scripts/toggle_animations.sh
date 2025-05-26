f23a291d691b59c14a3d7ad8af6a719354da1943
raceer <web.raceer@pm.me>
2025-05-26 13:43:33 +0300

    2025, W25

---
+++ hypr/.config/hypr/scripts/toggle_animations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+on=$(hyprctl -j getoption animations:enabled | jq --raw-output '.int')
+
+if [[ $on -eq 1 ]]; then
+    hyprctl keyword animations:enabled 0
+    # hyprctl notify -1 1000 "rgb(e06c75)" "Animations off"
+    notify-send Animations off
+else
+    hyprctl keyword animations:enabled 1
+    # hyprctl notify -1 1000 "rgb(98c379)" "Animations on"
+    notify-send Animations on
+fi
